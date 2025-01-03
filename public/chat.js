a296615a2373b5e9416e278183fe611660212475
Glitch (pine-lucky-bison) <none>
2025-01-03 18:42:18 +0000

    Before import of arturr0/CipherConnect2 without specified path ./public/chat.js:51770304/55

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('https://pine-lucky-bison.glitch.me');
 const baseUrl = window.location.origin;
 document.addEventListener('DOMContentLoaded', () => {
     const signal = new Audio('sound.mp3');
