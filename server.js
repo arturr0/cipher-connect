a2c2797e05837ca006a71b893408182e248e5552
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-02-19 22:05:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ app.set('view engine', 'pug');
 
 // Handle file upload route
 const io = require('socket.io')(server, { 
-    maxHttpBufferSize: 2 * 1024 * 1024,
+    maxHttpBufferSize: 1400 * 1024,
     debug: true  // Enable verbose logging
 });
 
 app.use('/uploads', express.static(uploadsDir)); // Serve images from the uploads directory
 //app.use('/uploads', express.static('uploads'));
 app.use(require('express-fileupload')({
-    limits: { fileSize: 2 * 1024 * 1024 } // 1 MB
+    limits: { fileSize: 1400 * 1024 } 
 }));
 // Create uploads directory if it doesn't exist
 if (!fs.existsSync(uploadsDir)) {
