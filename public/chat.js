07e23f5076c2cea6afbbf586ea8b455af704698c
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-02-19 17:10:24 +0100

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1393,7 +1393,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	socket.on('user info', ({ id, profileImage }) => {
 		console.log(`User ID: ${id}`);
 		if (profileImage != null && document.getElementById("initials")) document.getElementById("initials").remove();
-		else if (document.getElementById("initials")) {
+		else {
 			document.getElementById("initials").classList.remove('display');
 			document.getElementById("initials").style.visibility = 'visible';
 		}
